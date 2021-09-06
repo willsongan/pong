cb459e5b2f8c3ed661a95b838dd9745a443354b9
willsongan <willsongan30@gmail.com>
2021-09-06 21:42:39 +0700

    pisahin collider sama gambar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Pemain 1
+    public PlayerControl player1; // skrip
+    private Rigidbody2D player1Rigidbody;
+
+    // Pemain 2
+    public PlayerControl player2; // skrip
+    private Rigidbody2D player2Rigidbody;
+
+    // Bola
+    public BallControl ball; // skrip
+    private Rigidbody2D ballRigidbody;
+    private CircleCollider2D ballCollider;
+
+    // Skor maksimal
+    public int maxScore;
+
+    // Inisialisasi rigidbody dan collider
+    private void Start()
+    {
+        player1Rigidbody = player1.GetComponent<Rigidbody2D>();
+        player2Rigidbody = player2.GetComponent<Rigidbody2D>();
+        ballRigidbody = ball.GetComponent<Rigidbody2D>();
+        ballCollider = ball.GetComponent<CircleCollider2D>();
+    }
+
+
+}
